a1ec722c6a6333e89e42f8a0ef39f9a211fae55c
Nguyen Quoc Minh <minhnq49@ghtk.internal>
2021-12-20 10:01:04 +0700

    fix script create sound

--- sounds/create_msg_sound.sh
+++ sounds/create_msg_sound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 msg_text=`echo $1 | tr '_' ' '`
-google_speech -l vi "Cảnh báo: '$msg_text'" -o $1.mp3 && ffmpeg  -y -i $1.mp3  -f s16le -ac 1 -acodec pcm_s16le -ar 45000 ./sounds/msg/$1.raw  && rm -rf $2.mp3
+google_speech -l vi "Cảnh báo: '$msg_text'" -o $1.mp3 && ffmpeg  -y -i $1.mp3  -f s16le -ac 1 -acodec pcm_s16le -ar 45000 ./sounds/msg/$2.raw  && rm -rf $1.mp3
 #google_speech "Alert: '$msg_text'" -o $1.mp3 && ffmpeg  -y -i $1.mp3  -f s16le -ac 1 -acodec pcm_s16le -ar 45000 ./sounds/msg/$1.raw  && rm -rf $1.mp3
