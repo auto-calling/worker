66c5e260a734216a31d0e27650fad84bb11b3462
Nguyen Quoc Minh <minhnq49@ghtk.internal>
2021-12-20 09:56:19 +0700

    fix script create sound

--- sounds/create_host_sound.sh
+++ sounds/create_host_sound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 google_speech -l vi "Địa chỉ Server: '$1'" -o $1.mp3 
-ffmpeg -y  -i $1.mp3  -f s16le -ac 1 -acodec pcm_s16le -ar 45000 ./sounds/host/$1.raw 
+ffmpeg -y  -i $1.mp3  -f s16le -ac 1 -acodec pcm_s16le -ar 45000 ./sounds/host/$2.raw 
 rm -rf $1.mp3
